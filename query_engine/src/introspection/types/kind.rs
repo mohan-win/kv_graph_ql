88424a1d2ffa155081034db30820e6ab9966128b
Mohanraj K Rangasamy <mohan_win@outlook.com>
2024-11-18 17:32:05 +0530

    updated comments to match graphQL fields names.

--- query_engine/src/introspection/types/kind.rs
+++ query_engine/src/introspection/types/kind.rs
@@ -7,14 +7,14 @@ pub enum __TypeKind {
     Object,
     /// Indicates this type is an interface. `fields` and `possible_types` are valid fields.
     Interface,
-    /// Indicates this type is an union. `possible_types` is a valid field.
+    /// Indicates this type is an union. `possibleTypes` is a valid field.
     Union,
-    /// Indicates this type is an enum. `enum_values` is a valid field
+    /// Indicates this type is an enum. `enumValues` is a valid field
     Enum,
-    /// Indicates this type is an input object. `input_fields` is a valid field.
+    /// Indicates this type is an input object. `inputFields` is a valid field.
     InputObject,
-    /// Indicates this type is a list. `of_type` is a valid field.
+    /// Indicates this type is a list. `ofType` is a valid field.
     List,
-    /// Indicates this type is a non-null. `of_type` is a valid field.
+    /// Indicates this type is a non-null. `ofType` is a valid field.
     NonNull,
 }
