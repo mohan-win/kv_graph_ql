346c0b564fd5950734a10ef4036b53e4cd399739
Mohanraj K Rangasamy <mohan_win@outlook.com>
2024-11-27 19:07:48 +0530

    removed unnecessary tests.

--- query_engine/src/lib.rs
+++ query_engine/src/lib.rs
@@ -18,40 +18,4 @@ pub use graphql_value::{
 };
 
 #[cfg(test)]
-mod tests {
-  use super::*;
-  use std::fs;
-
-  #[derive(Serialize, Deserialize)]
-  struct GraphQLQuery {
-    query: String,
-    variables: String,
-  }
-
-  #[test]
-  fn test_executable_doc_ast() {
-    let graphql_query = GraphQLQuery {
-      query: fs::read_to_string(concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/test_data/crud_queries/create_user_with_posts.graphql"
-      ))
-      .unwrap(),
-      variables: fs::read_to_string(concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/test_data/crud_queries/create_user_with_posts.json"
-      ))
-      .unwrap(),
-    };
-    let graphql_schema = fs::read_to_string(concat!(
-      env!("CARGO_MANIFEST_DIR"),
-      "/test_data/test_crud_api_def.graphql"
-    ))
-    .unwrap();
-    // ToDo:: Validate query against schema.
-    // ToDo:: Validate variable values against schema.
-    let graphql_schema_ast = graphql_parser::parse_schema(graphql_schema).unwrap();
-    let executable_doc_ast = graphql_parser::parse_query(graphql_query.query);
-    eprintln!("{:#?}", executable_doc_ast);
-    assert!(false, "testing!!");
-  }
-}
+mod tests {}
