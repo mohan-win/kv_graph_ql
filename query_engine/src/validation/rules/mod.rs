c85902cc74e47ac196a064985b5356cb23fa6986
Mohanraj K Rangasamy <mohan_win@outlook.com>
2024-12-02 12:23:38 +0530

    implemented known_argument_names rule.

--- query_engine/src/validation/rules/mod.rs
+++ query_engine/src/validation/rules/mod.rs
@@ -2,8 +2,10 @@ mod arguments_of_correct_type;
 mod default_values_of_correct_type;
 mod directives_unique;
 mod fields_on_correct_type;
+mod known_argument_names;
 
 pub use arguments_of_correct_type::ArgumentsOfCorrectType;
 pub use default_values_of_correct_type::DefaultValuesOfCorrectType;
 pub use directives_unique::DirectivesUnique;
 pub use fields_on_correct_type::FieldsOnCorrectType;
+pub use known_argument_names::KnownArgumentNames;
