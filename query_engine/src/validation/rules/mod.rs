b8e65f4683ea4163a7a90e158fe3abc9e4cfcd3e
Mohanraj K Rangasamy <mohan_win@outlook.com>
2024-12-02 12:49:50 +0530

    implemented known_fragment_names rule.

--- query_engine/src/validation/rules/mod.rs
+++ query_engine/src/validation/rules/mod.rs
@@ -4,6 +4,7 @@ mod directives_unique;
 mod fields_on_correct_type;
 mod known_argument_names;
 mod known_directives;
+mod known_fragment_names;
 
 pub use arguments_of_correct_type::ArgumentsOfCorrectType;
 pub use default_values_of_correct_type::DefaultValuesOfCorrectType;
@@ -11,3 +12,4 @@ pub use directives_unique::DirectivesUnique;
 pub use fields_on_correct_type::FieldsOnCorrectType;
 pub use known_argument_names::KnownArgumentNames;
 pub use known_directives::KnownDirectives;
+pub use known_fragment_names::KnownFragmentNames;
