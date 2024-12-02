74d06d3af1eeb3c49f807725a4af1bce06d1cd7e
Mohanraj K Rangasamy <mohan_win@outlook.com>
2024-12-02 12:51:35 +0530

    implemented known_type_names rule.

--- query_engine/src/validation/rules/mod.rs
+++ query_engine/src/validation/rules/mod.rs
@@ -5,6 +5,7 @@ mod fields_on_correct_type;
 mod known_argument_names;
 mod known_directives;
 mod known_fragment_names;
+mod known_type_names;
 
 pub use arguments_of_correct_type::ArgumentsOfCorrectType;
 pub use default_values_of_correct_type::DefaultValuesOfCorrectType;
@@ -13,3 +14,4 @@ pub use fields_on_correct_type::FieldsOnCorrectType;
 pub use known_argument_names::KnownArgumentNames;
 pub use known_directives::KnownDirectives;
 pub use known_fragment_names::KnownFragmentNames;
+pub use known_type_names::KnownTypeNames;
