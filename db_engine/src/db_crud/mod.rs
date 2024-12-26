c86833712421e70bad478184f04b1a9a2ded0050
Mohanraj K Rangasamy <mohan_win@outlook.com>
2024-12-26 13:10:56 +0530

    moved types under types.

--- db_engine/src/db_crud/mod.rs
+++ db_engine/src/db_crud/mod.rs
@@ -10,26 +10,4 @@ pub use object::DBObject;
 pub use query::DBQuery;
 pub use types::*;
 
-/// ID of an object.
-pub struct ID(Arc<str>);
 
-/// Unique filter to search and find at most a object.
-pub struct ObjectWhereUniqueInput {}
-
-/// Filter to search and find more than one objects.
-pub struct ObjectWhereInput {}
-
-/// Objects order in result.
-pub enum ObjectOrderByInput {}
-
-/// All inputs needed to create a single object.
-pub struct ObjectCreateInput {}
-
-/// Type to capture the update data to update a single object.
-pub struct ObjectUpdateInput {}
-
-/// Type to capture the upsert data to either create or update a single object.
-pub struct ObjectUpsertInput {}
-
-/// Container to capture array objects, along with pagniation data.
-pub struct ObjectConnection {}
--- db_engine/src/db_crud/mutation.rs
+++ db_engine/src/db_crud/mutation.rs
@@ -1,4 +1,5 @@
 use crate::errors::Error;
+use async_trait::async_trait;
 
 use super::*;
 
